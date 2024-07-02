08c6ac3c6af93cd064b0f899f662bf9d43dfc963
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-07-02 23:51:58 +0200

    Add files via upload

---
+++ DirectoryManipulations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DirectoryManipulations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\48571\Downloads\Stworzone";
+            try
+            {
+                if(Directory.Exists(path))
+                {
+                    Console.WriteLine("This directory already exists!");
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(path);
+                    Console.WriteLine($"Directory succesfully created at {Directory.GetCreationTime(path).ToString("d MMMM yyyy, HH:mm:ss")}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failure has happened {ex.ToString}");
+            }
+        }
+    }
+}
